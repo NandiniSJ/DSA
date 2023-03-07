551cfc26a760e560368b39e970eebca0cee12e2a
NandiniSJ <nandinisjadhav97@gmail.com>
2023-03-07 22:55:06 +0530

    Added program to insert an element to sorted array

--- src/Arrays/InsertIntoSortedArray.java
+++ src/Arrays/InsertIntoSortedArray.java
@@ -1,18 +1,45 @@
 package Arrays;
 
+import java.util.Arrays;
 public class InsertIntoSortedArray {
 
-    public void insert(int[] array, int k, int low, int high){
-        if(high < low){
-            return;
+    public static int insert(int[] array, int k, int n, int capacity){
+        if(n > capacity){
+            return -1;
         }
-        int mid = (low + high)/ 2;
-        if(k == array[mid]){
-            for (int i = mid+1; i <=high ; i++) {
-                array[i+1] = array[i];
-            }
-            array[mid] = array[mid+1];
 
+        int i;
+        for(i = n-1; (i >=0 && array[i] > k); i--) {
+            array[i + 1] = array[i];
         }
+            array[i+1] = k;
+        return  n+1;
+
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[15];
+        array[0] = 0;
+        array[1] = 1;
+        array[2] = 2;
+        array[3] = 3;
+        array[4] = 5;
+        array[5] = 6;
+        array[6] = 6;
+        array[7] = 7;
+        array[8] = 9;
+        int capacity = array.length;
+        int n = 9;
+        int k = 8;
+
+        System.out.print("\nBefore Insertion: ");
+        for (int i = 0; i < n; i++)
+            System.out.print(array[i] + " ");
+
+        n = insert(array, k, n, capacity);
+
+        System.out.print("\nAfter Insertion: ");
+        for (int i = 0; i < n; i++)
+            System.out.print(array[i] + " ");
     }
 }
---
+++ src/JavaDeque.java
@@ -0,0 +1,18 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Deque {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Deque deque = new ArrayDeque();
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int num = in.nextInt();
+            deque.
+        }
+    }
+
+}
