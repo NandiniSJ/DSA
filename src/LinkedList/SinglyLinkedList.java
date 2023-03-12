2514d59084ce280a7216f5a5dbff91bdfbd384f6
NandiniSJ <nandinisjadhav97@gmail.com>
2023-03-12 22:18:29 +0530

    Added singly linked list program

---
+++ src/LinkedList/SinglyLinkedList.java
@@ -0,0 +1,62 @@
+package LinkedList;
+
+public class SinglyLinkedList<T> {
+
+    Node<T> head;
+
+    public void setHead(Node<T> head) {
+        this.head = head;
+    }
+
+    public void insert(T data){
+        Node<T> newNode = new Node<>(data,null);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+
+        Node<T> current = head;
+        while(current.getNext() != null){
+            current.setNext(current);
+        }
+        current.setNext(newNode);
+    }
+
+    public void insertFirst(T data){
+        Node<T> newNode = new Node<>(data,null);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+
+        newNode.setNext(head);
+        head = newNode;
+    }
+}
+
+class Node<T>{
+
+   private T data;
+    private Node<T> next;
+
+    public Node(T data, Node<T> next){
+        this.data = data;
+        this.next = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
